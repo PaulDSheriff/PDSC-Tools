6a065b0529ebaddf07f7ef232834a9ae04953b6e
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2022-02-25 14:39:22 -0600

    PDSC Developer Utilities
    PDSC Developer Utilities

--- PDSC-DeveloperUtilities/Templates/PDSC.Common/RepositoryInterfaces/IRepositoryBase.cs
+++ PDSC-DeveloperUtilities/Templates/PDSC.Common/RepositoryInterfaces/IRepositoryBase.cs
@@ -5,8 +5,10 @@ namespace PDSC.Common
   public interface IRepositoryBase<TEntity, TSearch>
   {
     IQueryable<TEntity> Search(TSearch search);
-    IQueryable<TEntity> AddWhereClause(IQueryable<TEntity> query, TSearch entity);
-    IQueryable<TEntity> AddOrderByClause(IQueryable<TEntity> query, TSearch entity);
+    IQueryable<TEntity> AddWhereClause(IQueryable<TEntity> query, TSearch search);
+    IQueryable<TEntity> AddOrderByClause(IQueryable<TEntity> query, TSearch search);
+    IQueryable<TEntity> ApplyPaging(IQueryable<TEntity> query, TSearch search);
+    int Count(TSearch search);
     TEntity CreateEmpty();
     TEntity Insert(TEntity entity);
     TEntity Update(TEntity entity);
