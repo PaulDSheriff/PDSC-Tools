7db818dd53958c2c01fc639cd6f4353be8d91e8d
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2022-03-04 08:23:38 -0600

    PDSC Developer Utilities
    PDSC Developer Utilities

---
+++ PDSC-DeveloperUtilities/Templates/DotNet6-MVC-SortingPaging/AppClasses/AppSession.cs
@@ -0,0 +1,35 @@
+﻿#nullable disable
+
+namespace <|NAMESPACE|>.Common {
+  /// <summary>
+  /// Use this class to hold your application session variables
+  /// NOTE: The CustomerId and CustomerName properties in here are examples only
+  /// </summary>
+  public class AppSession
+  {
+    #region Constructor
+    public AppSession(IHttpContextAccessor httpAccessor) : base()
+    {
+      HttpAccessor = httpAccessor;
+    }
+    #endregion
+
+    #region Fields
+    protected readonly IHttpContextAccessor HttpAccessor;
+    #endregion
+
+    #region Sample Properties Stored in Session
+    public int? CustomerId
+    {
+      get { return HttpAccessor.HttpContext.Session.GetInt32("CustomerId"); }
+      set { HttpAccessor.HttpContext.Session.SetInt32("CustomerId", value.Value); }
+    }
+
+    public string CustomerName
+    {
+      get { return HttpAccessor.HttpContext.Session.GetString("CustomerName"); }
+      set { HttpAccessor.HttpContext.Session.SetString("CustomerName", value); }
+    }
+    #endregion
+  }
+}
