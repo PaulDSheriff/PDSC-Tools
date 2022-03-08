2c9349d0ed843d2c1e3a2a47d74683a5b4785ece
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2022-03-08 15:26:11 -0600

    PDSC Developer Utilities
    PDSC Developer Utilities

---
+++ PDSC-DeveloperUtilities/Templates/Framework4-DataClasses.MVC/AppClasses/AppSession.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+
+namespace <|NAMESPACE|>.Common {
+  /// <summary>
+  /// Use this class to hold your application session variables
+  /// NOTE: The CustomerId and CustomerName properties in here are examples only
+  /// </summary>
+  public class AppSession {
+    #region Sample Properties Stored in Session
+    public int? CustomerId
+    {
+      get
+      {
+        if (HttpContext.Current.Session["CustomerId"] != null) {
+          return (int)HttpContext.Current.Session["CustomerId"];
+        }
+        else {
+          return null;
+        }
+      }
+      set { HttpContext.Current.Session["CustomerId"] = value.Value; }
+    }
+
+    public string CustomerName
+    {
+      get
+      {
+        if (HttpContext.Current.Session["CustomerName"] != null) {
+          return (string)HttpContext.Current.Session["CustomerName"];
+        }
+        else {
+          return null;
+        }
+      }
+      set { HttpContext.Current.Session["CustomerName"] = value; }
+    }
+    #endregion
+  }
+}
---
+++ PDSC-DeveloperUtilities/Templates/Framework4-PDSC.Common/BaseClasses/SearchBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PDSC.Common
+{
+  /// <summary>
+  /// All search classes should inherit from this class
+  /// </summary>
+  public class SearchBase
+  {
+    public SearchBase()
+    {
+      NoFilterAppliedMessage = "(none)";
+    }
+
+    /// <summary>
+    /// Get/Set any message to display after user performs a search
+    /// </summary>
+    public string NoFilterAppliedMessage { get; set; }
+  }
+}
