f40c1f67acdb4bac99e24f9a953e4e7161b681c8
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-10-24 16:29:53 -0500

    PDSC Developer Utilities V8.0.6
    PDSC Developer Utilities V8.0.6

---
+++ PDSC-DeveloperUtilities8/Templates/CodeGen-PDSC.Common.WPF/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PDSC.Common.WPF.Converters;
+
+public class InvertedBoolConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return !(bool)value;
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return new object();
+  }
+}
